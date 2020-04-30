8a23e3f023c71d135e93529199a456344963fe26
Sudeepto Dutta <sudeepto.dutta@tech9.com>
2020-04-30 14:30:54 +0530

    Added "new" and "create" actions for "article" resource :
    1. Whitlisted params under 'create' action that needs to be persisted in
       db.
    2. Once article is saved, redirected to 'show' page.
    3  Added "new.html.erb" file to show form data using "Rails form helper
       methods." Also added markup for showing success and error.
    4. Edited "application.html.erb" file to show app wise error messages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,32 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # Need to whitelist params that we need to persist in our database.
+    # Otherwise, we'll get ForbiddenAttributesError (Raised when forbidden attributes are used for mass assignment.)
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      # This line inspects the instance variable value to the HTML
+      # render plain: @article.inspect
+
+      # Using Rails helper method 'flash'
+
+      # Rails will extract the 'id' from '@article' instance variable and redirect the app to the newly created article show page.
+      # redirect_to article_path(@article)
+      # We can also write the above statement in a concise manner :
+      redirect_to @article, notice: "Articles created successfully"
+    else
+      render 'new'
+    end
   end
 end
