88d2c6e1ea07fb13ee22198b84bd689ff2bdd604
Sudeepto Dutta <sudeepto.dutta@tech9.com>
2020-05-01 17:17:49 +0530

    Added user model validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: {minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = URI::MailTo::EMAIL_REGEXP
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
